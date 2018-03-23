d743f32a93a26e2f49186fb14d747ebd9549509c
Olivier Lefebvre <olivier.lefebvre@live.com>
2018-03-23 14:24:22 +0100

    Update VSIXInstaller path

--- build/install-blazor.cmd
+++ build/install-blazor.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe" /q /a Microsoft.VisualStudio.BlazorExtension.vsix
+"C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\Common7\IDE\VSIXInstaller.exe" /q /a Microsoft.VisualStudio.BlazorExtension.vsix
